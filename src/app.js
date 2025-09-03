8d416e934e4e2ca840f0aee69eb2a4c2f9d837a2
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-09-03 14:13:58 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+
+function copying(locationForCopy, locationForPaste) {
+  if (locationForCopy === locationForPaste) {
+    return;
+  }
+
+  if (!locationForCopy || !locationForPaste) {
+    console.error('Not enough arguments provided');
+
+    return;
+  }
+
+  fs.cp(locationForCopy, locationForPaste, (err) => {
+    if (err) {
+      console.error('Something went wrong!');
+    }
+  });
+}
+
+copying(args[0], args[1]);
